198fec91c4fa66c875bd9077a34b283db7754801
rocket-pocket <verdudaniel7@gmail.com>
2018-08-19 12:13:48 +0200

    js

--- app.js
+++ app.js
@@ -2,21 +2,53 @@ class App{
     constructor(){
         this.btnAbrirMenu = document.querySelector('.abrirMenu')
         this.menuDesp = document.querySelector('.menu-hide')
-        this.btnWeb = document.querySelector('.contacto')
+        this.btnVerMas = document.querySelector('.btn_mas')
+        this.hiddenBoxes = document.querySelectorAll('.hidden')
+        
+
+        this.btnContacto = document.querySelector('#btn-contacto')/*  */
+        this.btnMusigrama = document.querySelector('#btn-musigrama')/*  */
+        this.btnBaby = document.querySelector('#btn-baby')/*  */
+        
 
         this.btnAbrirMenu.addEventListener('click', this.mostrarMenu.bind(this))
+        this.btnVerMas.addEventListener('click', this.mostrarCajas.bind(this))
+
+        this.btnContacto.addEventListener('click', this.openInNewTab.bind(this))/*  */
+        this.btnMusigrama.addEventListener('click', this.openMusigrama.bind(this))/*  */
+        this.btnBaby.addEventListener('click', this.openMusigrama.bind(this))/*  */
+        
     }
     mostrarMenu(){
         this.menuDesp.classList.toggle('menu-hide')
     }
-    mostrarEnlace(){
-        
-            this.btnWeb.location.assign("https://www.w3schools.com");
+    
+    mostrarCajas(){
+        this.hiddenBoxes.classList('hidden')
+        this.btnVerMas.innerHTML = '-';
+    }
+
+
+
+    openInNewTab() {/*  */
+        let url = '#port'
+        let win = window.open(url, /* '_blank' */)
+        win.focus();
+    }
+
+    openMusigrama(){/*  */
+        let url = ''
+        let win = window.open(url, '_blank')
+        win.focus()
+        if(this.btnMusigrama == this.btnMusigrama){
+            url = 'http://www.musigrama.com'
+        }
+        if(btn == this.btnBaby){
+            url = 'http://www.babyballet.es'
+        }
         
     }
+   
 }
-
-
-
 
 document.addEventListener('DOMContentLoaded', () => { new App()})

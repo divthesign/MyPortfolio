f4015052a582acc5f6c294372e3078a0ee5f879b
rocket-pocket <verdudaniel7@gmail.com>
2018-08-22 20:54:15 +0200

    retoques mediaqueris

--- app.js
+++ app.js
@@ -3,47 +3,25 @@ class App{
         this.menuHam = document.querySelector('.change') 
         this.btnAbrirMenu = document.querySelector('.abrirMenu')
         this.menuDesp = document.querySelector('.menu-hide')
-        
+        this.btnAbrirPort = document.querySelector('.btn_mas')
+        this.cajasPort = document.querySelector('.hidden')      
 
-        /* this.btnContacto = document.querySelector('#btn-contacto')
-        this.btnMusigrama = document.querySelector('#btn-musigrama')
-        this.btnBaby = document.querySelector('#btn-baby') */
+        
         
 
         this.btnAbrirMenu.addEventListener('click', this.mostrarMenu.bind(this))
-       
+        this.btnAbrirPort.addEventListener('click', this.mostrarPort.bind(this))
 
-        /*this.btnContacto.addEventListener('click', this.openInNewTab.bind(this))
-        this.btnMusigrama.addEventListener('click', this.openMusigrama.bind(this))
-        this.btnBaby.addEventListener('click', this.openMusigrama.bind(this))  */
         
     }
     mostrarMenu(){
         this.menuDesp.classList.toggle('menu-hide')
         this.menuHam.classList.toggle('change')
     }
+    mostrarPort(){
+        this.cajasPort.classList.toggle('hidden')
+    }
     
-
-
-
-   /*  openInNewTab() {
-        let url = '#port'
-        let win = window.open(url,  '_blank')
-        win.location();
-    } */
-
-    /* openMusigrama(){
-        let url = ''
-        let win = window.open(url, '_blank')
-        win.focus()
-        if(this.btnMusigrama == this.btnMusigrama){
-            url = 'http://www.musigrama.com'
-        }
-        if(btn == this.btnBaby){
-            url = 'http://www.babyballet.es'
-        }
-        
-    } */
    
 }
 

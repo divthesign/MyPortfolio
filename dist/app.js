1aacf0e25eb5ad8c0f3e56150486cc918c90cca7
rocket-pocket <verdudaniel7@gmail.com>
2018-10-21 09:14:58 +0200

    app funcion close menu y mail

--- dist/app.js
+++ dist/app.js
@@ -1,82 +1,73 @@
-class Menu {
+class app {
     constructor() {
-        console.log('hla');
-        this.menuHam = document.querySelector('.change');
-        this.btnAbrirMenu = document.querySelector('.abrirMenu');
-        this.menuDesp = document.querySelector('.menu-hide');
-        this.btnAbrirMenu.addEventListener('click', this.mostrarMenu.bind(this))
-    };
-    mostrarMenu() {
-        this.menuDesp.classList.toggle('menu-hide');
-        this.menuHam.classList.toggle('change')
-        console.log(this.menuHam)
-    }
-}
-class CerrarMenu{
-    constructor(){
+        console.log('hola bienvenido a mi portfolio')
+        this.menuHam = document.querySelector('.change')
+        this.btnAbrirMenu = document.querySelector('.abrirMenu')
+        this.menuDesp = document.querySelector('.menu-hide')
+
         this.link = document.querySelector('.menu-hide ul li a')
-        console.log(this.link)
         this.linkPort = document.querySelector('#link-port')
         this.linkSkills = document.querySelector('#link-skills')
-        this.menuDesp = document.querySelector('.menu-hide');
+        this.menuDesp = document.querySelector('.menu-hide')
         this.menuHamOpen = document.querySelector('.change')
         this.btnContacto = document.querySelector('#btn-contacto')
-        this.link.addEventListener('click', this.collapseMenu.bind(this))
-        this.linkPort.addEventListener('click', this.collapseMenuDos.bind(this))
-        this.linkSkills.addEventListener('click', this.collapseMenuTres.bind(this))
-        this.btnContacto.addEventListener('click', this.collapseMenuCuatro.bind(this))
+
+        this.nav = document.querySelector('nav')
+        this.nav.addEventListener('click', this.collapseMenu.bind(this))
+        
+
+        this.btnAbrirPort = document.querySelector('.btn_mas')
+        this.cajasPort = document.querySelectorAll('.hidden')
+
+        this.btnTop = document.querySelector('#btn-top')
+        this.divTop = document.querySelector('#div-top')
+        this.header = document.querySelector('header')
+        this.h1Header = document.querySelector('header h1')
+        this.bajoHeader = document.querySelector('#bajo-header')
+
+        this.btnContacto = document.querySelector('#btn-contacto')
+
+        this.myMail = document.querySelector('.my-mail')
+
+
+        this.btnAbrirMenu.addEventListener('click', this.mostrarMenu.bind(this))
+        this.btnAbrirPort.addEventListener('click', this.mostrarPort.bind(this))
+        document.addEventListener('scroll', this.scrollDetect.bind(this))
+        this.btnTop.addEventListener('click', this.subir.bind(this))
+        window.addEventListener('scroll', this.scrollSection1.bind(this))
+        window.addEventListener('scroll', this.scrollSection2.bind(this))
+        window.addEventListener('scroll', this.scrollSection3.bind(this))
+        this.btnContacto.addEventListener('click', this.contacto.bind(this))
+
+    
+        this.myMail.addEventListener('click', this.SendMeAMail.bind(this) )
+    
+    }
+    SendMeAMail(){
+                const url = 'mailto@:divthesign@gmail.com'
+                window.open(`http://${url}`)
+    }
+    mostrarMenu() {
+        this.menuDesp.classList.toggle('menu-hide')
+        this.menuHam.classList.toggle('change')
     }
     collapseMenu(){
-        this.menuDesp.classList.toggle('menu-hide')
-        this.menuHamOpen.classList.toggle('change')
-        console.log(this.menuHamOpen)
-    }
-    collapseMenuDos(){
-        console.log('collapse do funciona')
-        this.menuDesp.classList.toggle('menu-hide')
-        this.menuHamOpen.classList.toggle('change')
-    }
-    collapseMenuTres(){
-        console.log('collapse do funciona')
-        this.menuDesp.classList.toggle('menu-hide')
-        this.menuHamOpen.classList.toggle('change')
+        if(this.menuDesp){
+            this.menuDesp.classList.toggle('menu-hide')
+            this.menuHamOpen.classList.toggle('change') 
+        }
+        
         
     }
-    collapseMenuCuatro(){
-        this.menuDesp.classList.toggle('menu-hide')
-        this.menuHamOpen.classList.toggle('change') 
-    }
-}
-new CerrarMenu()
-class Portfolio {
-    constructor() {
-        this.btnAbrirPort = document.querySelector('.btn_mas');
-        this.cajasPort = document.querySelectorAll('.hidden');
-        this.btnAbrirPort.addEventListener('click', this.mostrarPort.bind(this))
-    };
     mostrarPort() {
-        console.log('hola');
         this.cajasPort.forEach((item) => {
             item.classList.toggle('hidden')
         })
     }
-    
-}
-new Portfolio()
-class Arriba {
-    constructor() {
-        this.btnTop = document.querySelector('#btn-top');
-        this.divTop = document.querySelector('#div-top');
-        this.header = document.querySelector('header');
-        this.h1Header = document.querySelector('header h1');
-        this.bajoHeader = document.querySelector('#bajo-header');
-        document.addEventListener('scroll', this.scrollDetect.bind(this));
-        this.btnTop.addEventListener('click', this.subir.bind(this))
-    };
     scrollDetect(oE) {
         if (oE.target.scrollingElement.scrollTop > 20) {
-            this.h1Header.classList.add('hide');
-            this.bajoHeader.classList.add('bajo-header');
+            this.h1Header.classList.add('hide')
+            this.bajoHeader.classList.add('bajo-header')
             this.bajoHeader.classList.remove('hide-scroll')
         }
         if (oE.target.scrollingElement.scrollTop > 100) {
@@ -84,7 +75,7 @@ class Arriba {
         } else {
             this.divTop.classList.add('hide-scroll')
         }
-    };
+    }
     subir(oE) {
         window.scroll({
             top: 0,
@@ -92,69 +83,48 @@ class Arriba {
             behavior: 'smooth'
         })
     }
-}
-new Arriba()
-class FadeDownUno {
-    constructor() {
-        window.addEventListener('scroll', this.scrollSection1.bind(this))
-    };
     scrollSection1() {
-        this.header = document.querySelector('header');
-        this.scrollSection = document.querySelector('section');
-        console.log(this.scrollSection);
-        this.ypos = window.pageYOffset;
+        this.header = document.querySelector('header')
+        this.scrollSection = document.querySelector('section')
+        this.ypos = window.pageYOffset
         if (this.ypos > 187) {
             this.scrollSection.classList.remove('about')
         } else {
             this.scrollSection.classList.add('about')
         }
     }
-}
-new FadeDownUno()
-class FadeDownDos {
-    constructor() {
-        window.addEventListener('scroll', this.scrollSection1.bind(this))
-    };
-    scrollSection1() {
-        this.header = document.querySelector('header');
-        this.scrollSection = document.querySelector('.port');
-        console.log(this.scrollSection);
-        this.ypos = window.pageYOffset;
+    scrollSection2() {
+        this.header = document.querySelector('header')
+        this.scrollSection = document.querySelector('.port')
+        console.log(this.scrollSection)
+        this.ypos = window.pageYOffset
         if (this.ypos > 1437) {
             this.scrollSection.classList.remove('about')
         } else {
             this.scrollSection.classList.add('about')
         }
     }
-}
-new FadeDownDos()
-class FadeDownTres {
-    constructor() {
-        window.addEventListener('scroll', this.scrollSection1.bind(this))
-    };
-    scrollSection1() {
+    scrollSection3() {
         this.header = document.querySelector('header')
         this.scrollSection = document.querySelector('#skills')
-        console.log(this.scrollSection);
-        this.ypos = window.pageYOffset;
+        console.log(this.scrollSection)
+        this.ypos = window.pageYOffset
         if (this.ypos > 2687) {
             this.scrollSection.classList.remove('about')
         } else {
             this.scrollSection.classList.add('about')
         }
     }
-}
-new FadeDownTres
-class Contacto {
-    constructor() {
-        this.btnContacto = document.querySelector('#btn-contacto')
-        this.btnContacto.addEventListener('click', this.contacto.bind(this))
-    };
     contacto() {
-           this.sectionContacto = document.querySelector('#contactame', location.assign('#contactame'))         
-    }
-}
-new Contacto()  
+        this.sectionContacto = document.querySelector('#contactame', 
+        location.assign('#contactame'))
+        }        
+ }
+ 
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    new Menu()
+    new app()
 })

10d26662faea44947a605104123d478f4aa496de
rocket-pocket <verdudaniel7@gmail.com>
2018-10-21 11:41:00 +0200

    gulp minify working

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 const minify = require('gulp-minify');
+const cleanCSS = require('gulp-clean-css');
+const minifyHTML = require('gulp-htmlmin')
 
 gulp.task('default', function() {
   // place code for your default task here
-});
- 
-gulp.task('compress', function() {
-  gulp.src(['lib/*.js', 'lib/*.mjs'])
+}); 
+gulp.task('compressJs', function() {
+  gulp.src(['src/*.js', 'src/*.mjs'])
     .pipe(minify())
     .pipe(gulp.dest('dist'))
 });
+
+gulp.task('minify-css', () => {
+  return gulp.src('dist/css/*.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+  .pipe(gulp.dest('dist'));
+});
+gulp.task('minify-html', () => {
+  return gulp.src('*.html')
+    .pipe(minifyHTML({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+gulp.task('default', ['compressJs', 'minify-css', 'minify-html']);
+
+
+
